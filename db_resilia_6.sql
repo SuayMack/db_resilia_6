6a75949700929ad9aa39522ffa48aa24f4088ea6
SuayMack <pry_chu@hotmail.com>
2023-08-10 19:49:07 -0300

    CREATE - novas tabelas

--- db_resilia_6.sql
+++ db_resilia_6.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE IF NOT EXISTS resilia_6;
-
 USE resilia_6;
-
 -- Tabela PessoaFacilitadora
 CREATE TABLE IF NOT EXISTS PessoaFacilitadora (
   id_facilitador INT AUTO_INCREMENT PRIMARY KEY,
@@ -11,7 +9,6 @@ CREATE TABLE IF NOT EXISTS PessoaFacilitadora (
   formacao VARCHAR(100),
   experiencia VARCHAR(200)
 );
-
 -- Tabela Curso
 CREATE TABLE IF NOT EXISTS Curso (
   id_curso INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,60 +25,19 @@ CREATE TABLE IF NOT EXISTS Turma (
   id_curso INT,
   nome VARCHAR(100) NOT NULL,
   data_inicio DATE,
-<<<<<<< HEAD
-  data_termino DATE,
-  id_curso INT,
-  FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
-);
-
--- Tabela Modulo
-CREATE TABLE IF NOT EXISTS Modulo (
-  id_modulo INT AUTO_INCREMENT PRIMARY KEY,
-  id_facilitador INT,
-  nome VARCHAR(100) NOT NULL,
-  descricao VARCHAR(200),
-  carga_horaria INT,
-  id_curso INT,
-  FOREIGN KEY (id_curso) REFERENCES Curso(id_curso),
-  FOREIGN KEY (id_facilitador) REFERENCES PessoaFacilitadora(id_facilitador)
-=======
   data_termino DATE,  
   FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
->>>>>>> ad55c62e8d60777ba48af1032f92bcd16d955f21
 );
 
 -- Tabela Estudante
 CREATE TABLE IF NOT EXISTS Estudante (
   id_estudante INT AUTO_INCREMENT PRIMARY KEY,
-  id_modulo INT,
   nome VARCHAR(100) NOT NULL,
   data_nascimento DATE,
   email VARCHAR(100) NOT NULL,
   status VARCHAR(20),
   id_turma INT,
-  FOREIGN KEY (id_turma) REFERENCES Turma(id_turma),
-  FOREIGN KEY (id_modulo) REFERENCES Modulo(id_modulo)
-);
-
-CREATE TABLE  IF NOT EXISTS FinanceiroEstudante (
-    id_financeiro INT AUTO_INCREMENT PRIMARY KEY,
-    id_estudante INT,
-    id_curso INT,
-    valor_total DECIMAL(10, 2),
-    valor_desconto DECIMAL(10, 2),
-    valor_pago DECIMAL(10, 2),
-    status_pagamento VARCHAR(50),
-    FOREIGN KEY (id_estudante) REFERENCES Estudante(id_estudante),
-    FOREIGN KEY (id_curso) REFERENCES Curso(id_curso)
-
-);
-
-CREATE TABLE IF NOT EXISTS FinanceiroResilia (
-    id_despesas INT AUTO_INCREMENT PRIMARY KEY,
-    id_facilitador INT,
-    tipo_despesa VARCHAR(50),
-    valor_despesa DECIMAL(10, 2),
-    FOREIGN KEY (id_facilitador) REFERENCES PessoaFacilitadora(id_facilitador)
+  FOREIGN KEY (id_turma) REFERENCES Turma(id_turma)
 );
 
 -- Tabela Modulo
@@ -136,4 +92,3 @@ CREATE TABLE IF NOT EXISTS FinanceiroFacilitador (
   FOREIGN KEY (id_facilitador) REFERENCES PessoaFacilitadora(id_facilitador),
   FOREIGN KEY (id_aula) REFERENCES AulasDadas(id_aula)
 );
-
